c63b80a8e2075cc8ecb6dc36b7cdc4f9c8eea6d1
thomero <93338886+thomero@users.noreply.github.com>
2026-09-02 13:16:01 +0300

    fix: make Quit command exit desktop app

--- packages/cad-viewer-example/src/commands/quitCmd.ts
+++ packages/cad-viewer-example/src/commands/quitCmd.ts
@@ -1,12 +1,16 @@
 import { AcEdCommand } from '@mlightcad/cad-simple-viewer'
 
+import { exitDesktopApp, isDesktopHost } from '../desktop'
 import { store } from '../store'
 
-/**
- * Command to close cad viewer
- */
+/** Command to close the CAD viewer or native Windows application. */
 export class AcApQuitCmd extends AcEdCommand {
   async execute() {
+    if (isDesktopHost()) {
+      await exitDesktopApp()
+      return
+    }
+
     store.selectedFile = null
     store.isNewDrawing = false
   }
