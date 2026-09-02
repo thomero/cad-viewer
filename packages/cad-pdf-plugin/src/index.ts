7207b6f432ebbc0f8ac26af24b28a55559839e19
thomero <93338886+thomero@users.noreply.github.com>
2026-09-02 15:01:52 +0300

    Export PDF plot settings API

--- packages/cad-pdf-plugin/src/index.ts
+++ packages/cad-pdf-plugin/src/index.ts
@@ -8,5 +8,17 @@ export { AcApConvertToPdfCmd } from './AcApConvertToPdfCmd'
 export { AcApImportPdfCmd } from './AcApImportPdfCmd'
 export { AcApPdfConvertor } from './AcApPdfConvertor'
 export { AcApPdfImportConvertor } from './AcApPdfImportConvertor'
+export {
+  DEFAULT_PDF_PLOT_SETTINGS,
+  normalizePdfPlotBounds
+} from './AcApPdfPlotSettings'
+export type {
+  AcApPdfOrientation,
+  AcApPdfPaperSize,
+  AcApPdfPlotArea,
+  AcApPdfPlotBounds,
+  AcApPdfPlotSettings,
+  AcApPdfScaleMode
+} from './AcApPdfPlotSettings'
 export { createPdfPlugin } from './createPdfPlugin'
 export { PDF_PLUGIN_NAME, PDF_PLUGIN_TRIGGERS } from './register'
